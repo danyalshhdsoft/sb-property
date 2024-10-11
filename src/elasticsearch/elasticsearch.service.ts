7d7fd663863d2ce26852c42ff67aa54d5a073f85
softbuilders <danyalshhdsoft@gmail.com>
2024-10-11 18:31:37 +0400

    feat(search): implemented autocomplete and search (#1)

--- src/elasticsearch/elasticsearch.service.ts
+++ src/elasticsearch/elasticsearch.service.ts
@@ -23,11 +23,31 @@ export class ElasticsearchService {
     });
   }
 
-  async search(index: string, query: any) {
+  async search(query: any, from: number, size: number = 10, index: string = 'properties') {
     const body = await this.elasticsearchService.search({
       index,
       body: query,
+      size,
+      from
     });
     return body.hits.hits;
+  }
+
+  async searchAutocomplete(query: string, index: string = 'properties') {
+    const body = await this.elasticsearchService.search({
+      index,
+      body: {
+        query: {
+          multi_match: {
+            query,
+            type: "phrase_prefix",
+            fields: ["area", "building"],
+            slop: 1
+          }
+        },
+        size: 10, // Limit results for autocomplete
+      },
+    });
+    return { status: 200, data: body.hits.hits.map(hit => hit._source) }
   }
 }
