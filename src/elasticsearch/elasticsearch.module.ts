5e4287a1e6397f0474b933e9d7c32f773f7d1e55
softbuilders <danyalshhdsoft@gmail.com>
2024-10-14 14:22:50 +0400

    feat(property): update and delete property in search (#2)

--- src/elasticsearch/elasticsearch.module.ts
+++ src/elasticsearch/elasticsearch.module.ts
@@ -1,19 +1,26 @@
 import { Module } from '@nestjs/common';
 import { ElasticsearchModule } from '@nestjs/elasticsearch';
 import { ElasticsearchService } from './elasticsearch.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    ElasticsearchModule.register({
-      node: 'https://localhost:9200', // Your Elasticsearch URL windows
-      // node: 'http://localhost:9200', // Your Elasticsearch URL linux
-      tls: {
-        rejectUnauthorized: false,
-      },
-      auth: {
-        username: 'elastic',
-        password: 'xmiAu67d7V7aVoWud0JD',
-      },
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes the config module globally available
+    }),
+    ElasticsearchModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        node: configService.get<string>('ELASTICSEARCH_NODE'),
+        auth: {
+          username: configService.get<string>('ELASTICSEARCH_USERNAME'),
+          password: configService.get<string>('ELASTICSEARCH_PASSWORD'),
+        },
+        tls: {
+          rejectUnauthorized: (String(configService.get<boolean>('ELASTICSEARCH_REJECT_UNAUTHORIZED')).toLowerCase() === 'true'),
+        },
+      }),
     }),
   ],
   providers: [ElasticsearchService],
